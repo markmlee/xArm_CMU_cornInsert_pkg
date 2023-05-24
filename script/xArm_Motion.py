a1252427b57805290c761a5b2c879e8f7e8a5e0b
marklee <lmy0510@gmail.com>
2023-05-23 21:51:47 -0400

    can do motion for both sides

--- script/xArm_Motion.py
+++ script/xArm_Motion.py
@@ -36,41 +36,50 @@ class xArm_Motion():
 
     def go_to_home(self):
         print(" ---- going to home position ----")
-        # self.arm.move_gohome()
+        self.arm.move_gohome()
 
     def go_to_plane(self):
         print(" ---- going to plane joint position ----")
-        # self.arm.set_servo_angle(angle=[0, -28.2, -98.5, 0, 126.6, 0], is_radian=False, wait=True)
+        self.arm.set_servo_angle(angle=[0, -28.2, -98.5, 0, 126.6, 0], is_radian=False, wait=True)
 
     def go_to_rotated_plane(self):
         print(f" ---- rotating EE 90 deg  ----")
-        # self.arm.set_position_aa(axis_angle_pose=[0, 0, 0, -90, 0, 0], relative=True, wait=True)
+        self.arm.set_position_aa(axis_angle_pose=[0, 0, 0, -90, 0, 0], relative=True, wait=True)
 
-    def go_to_rotate_joint6(self):
+    def go_to_rotated_plane_right(self):
+        print(f" ---- rotating EE 90 deg  ----")
+        self.arm.set_position_aa(axis_angle_pose=[0, 0, 0, 90, 0, 0], relative=True, wait=True)
+
+
+    def go_to_rotate_joint6(self, deg):
         print(f" ---- rotate joint 6 ----")
-        # self.arm.set_servo_angle(angle=[0,0,0,0,0, 60], relative=True, is_radian=False, wait=True)
+        self.arm.set_servo_angle(angle=[0,0,0,0,0, deg], relative=True, is_radian=False, wait=True)
 
 
     def go_to_approach_corn_left(self):
         print(f" ---- go to corn approach ----")
-        # self.arm.set_position_aa(axis_angle_pose=[0, -230, 0, 0, 0, 0], relative=True, wait=True)
+        self.arm.set_position_aa(axis_angle_pose=[0, -230, 0, 0, 0, 0], relative=True, wait=True)
+    
+    def go_to_approach_corn(self, y_mm):
+        print(f" ---- go to corn approach ----")
+        self.arm.set_position_aa(axis_angle_pose=[0, y_mm, 0, 0, 0, 0], relative=True, wait=True)
     
 
     def go_to_inside_corn_left(self):
         print(f" ---- inside to corn approach ----")
-        # self.arm.set_position_aa(axis_angle_pose=[-80, 0, 0, 0, 0, 0], relative=True, wait=True)    
+        self.arm.set_position_aa(axis_angle_pose=[-80, 0, 0, 0, 0, 0], relative=True, wait=True)    
 
     def go_to_outside_corn_left(self):
         print(f" ---- outside to corn approach ----")
-        # self.arm.set_position_aa(axis_angle_pose=[80, 0, 0, 0, 0, 0], relative=True, wait=True)    
+        self.arm.set_position_aa(axis_angle_pose=[80, 0, 0, 0, 0, 0], relative=True, wait=True)    
 
     def go_to_plane_back(self):
         print(f" ---- go to plane position back ----")
-        # self.arm.set_position_aa(axis_angle_pose=[0, 230, 0, 0, 0, 0], relative=True, wait=True)
+        self.arm.set_position_aa(axis_angle_pose=[0, 230, 0, 0, 0, 0], relative=True, wait=True)
 
     def go_to_rotate_joint6_back(self):
         print(f" ---- rotate joint 6 back ----")
-        # self.arm.set_servo_angle(angle=[0,0,0,0,0, -60], relative=True, is_radian=False, wait=True)
+        self.arm.set_servo_angle(angle=[0,0,0,0,0, -90], relative=True, is_radian=False, wait=True)
  
 
 
@@ -89,7 +98,7 @@ class xArm_Motion():
         self.go_to_rotated_plane()
 
         # rotate joint 6 to align w left corn
-        self.go_to_rotate_joint6()
+        self.go_to_rotate_joint6(90)
 
         # approach corn with offset
         self.go_to_approach_corn_left()
@@ -99,18 +108,57 @@ class xArm_Motion():
 
         time.sleep(3)
 
-        # move back out of corn
+        # # move back out of corn
         self.go_to_outside_corn_left()
 
-        # return to plane position
-        self.go_to_plane_back()
-        self.go_to_rotate_joint6_back()
+        # # return to plane position
+        # self.go_to_plane_back()
+        # self.go_to_rotate_joint6_back()
 
-        # last joint motion
+        # # last joint motion
+        # self.go_to_plane()
+
+        # # return to home position
+        # self.go_to_home()
+
+    def simple_blind_insert_motions_rightside(self):
+        """
+        series of motions for corn insertion without any sensor feedback. Solely to visually test motion 
+        """
+
+        # go to home position
+        self.go_to_home()
+
+        # go to above plane
         self.go_to_plane()
 
-        # return to home position
+        # rotate to align w left corn
+        self.go_to_rotated_plane_right()
+
+        # # rotate joint 6 to align w left corn
+        self.go_to_rotate_joint6(-90)
+
+        # # approach corn with offset
+        self.go_to_approach_corn(230)
+
+        # # move inside corn
+        self.go_to_inside_corn_left()
+
+        time.sleep(3)
+
+        # # move back out of corn
+        self.go_to_outside_corn_left()
+
+        # # return to plane position
+        self.go_to_approach_corn(-230)
+        self.go_to_rotate_joint6(90)
+
+        # # last joint motion
+        self.go_to_plane()
+
+        # # return to home position
         self.go_to_home()
+
 
     
 
