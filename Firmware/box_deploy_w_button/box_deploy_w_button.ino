ee9889138e11a7ecf035b166289dbec1cc2a2a0e
Ben Tarr <btarr@andrew.cmu.edu>
2023-06-06 15:32:05 -0400

    rewrite arduino firmware

--- Firmware/box_deploy_w_button/box_deploy_w_button.ino
+++ Firmware/box_deploy_w_button/box_deploy_w_button.ino
@@ -1,134 +1,124 @@
 #include <Servo.h>
 
-Servo myservo;  
-Servo myservo2;
-Servo myservo3;
-Servo myservo4;
-Servo myservo5;
+// Define a structure to encapsulate the details of a "box"
+struct logger {
+  Servo servo;        // the servo associated with the box
+  int boxState = -1;  // current state of the box (-1=closed, 1=open)
+};
 
-const int buttonPin1 = 2;
-const int buttonPin2 = 3;
-const int buttonPin3 = 4;
-const int buttonPin4 = 5;
-const int buttonPin5 = 6;
+const int NUMBER_OF_BOXES = 5;  // Define the total number of boxes
 
-int buttonState1 = 0;
-int buttonState2 = 0;
-int buttonState3 = 0;
-int buttonState4 = 0;
-int buttonState5 = 0;
+logger boxes[NUMBER_OF_BOXES];  // Create an array of boxes
 
-long lastDebounceTime = 0;  // the last time the output pin was toggled
-long debounceDelay = 2000;    // the debounce time; increase if the output flickers
+// Array to hold button pin numbers
+int buttonPins[NUMBER_OF_BOXES] = { 2, 3, 4, 5, 6 };
 
-int boxState1 = -1;
-int boxState2 = -1;
-int boxState3 = -1;
-int boxState4 = -1;
-int boxState5 = -1;
+// Array to hold servo pin numbers
+int servoPins[NUMBER_OF_BOXES] = { 11, 12, 9, 10, 8 };
+// Variables to control debouncing
+
+long debounceDelay = 50;  // the debounce time; increase if the output flickers
+
+struct button {
+  long lastDebounceTime = 0;  // the last time the output pin was toggled
+  int pin;                    // the pin for the button that controls the box
+  int lastState = 0;          // last state of the button (0=unpressed, 1=pressed)
+  int state = 0;
+  int reading;
+};
+
+button buttons[NUMBER_OF_BOXES];
+
+// A buffer to hold incoming commands from the Serial input
+char inputBuffer[200];
+int bufferIndex = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  myservo.attach(8);  
-  myservo2.attach(9);
-  myservo3.attach(10);
-  myservo4.attach(11);
-  myservo5.attach(12);
-
-  myservo.write(55);
-  myservo2.write(55);
-  myservo3.write(55);
-  myservo4.write(55);
-  myservo5.write(55);  
-
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
-  pinMode(buttonPin4, INPUT);
-  pinMode(buttonPin5, INPUT);
+  // Loop over each box to set it up
+  for (int i = 0; i < NUMBER_OF_BOXES; i++) {
+    buttons[i].pin = buttonPins[i];       // assign the button pin from the array
+    boxes[i].servo.attach(servoPins[i]);  // attach the servo to the pin from the array
+    boxes[i].servo.write(55);             // move the servo to its initial position
+    pinMode(buttons[i].pin, INPUT);   // set the button's pin mode to INPUT
+  }
+  Serial.begin(9600);  // begin serial communication
 }
 
+
 void loop() {
+  // Check if any data is available on the Serial input
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
 
-  if ( (millis() - lastDebounceTime) > debounceDelay) {
+    // If newline character is received, a full command is ready for parsing
+    if (inChar == '\n') {
+      inputBuffer[bufferIndex] = '\0';  // Null-terminate the C string
 
-    buttonState1 = digitalRead(buttonPin1);
-    buttonState2 = digitalRead(buttonPin2);
-    buttonState3 = digitalRead(buttonPin3);
-    buttonState4 = digitalRead(buttonPin4);
-    buttonState5 = digitalRead(buttonPin5);
-    
-    if ( (buttonState1 == 1) && (boxState1 < 0) ) {
-      Serial.println("open door 1");
-      myservo.write(0); //open the box
-      boxState1 = -boxState1; //change the state of box
-      lastDebounceTime = millis(); //set the current time      
-    }
-    else if ( (buttonState1 == 1) && (boxState1 > 0) ) {
-      Serial.println("closed door 1");
-      myservo.write(55); //close the box
-      boxState1 = -boxState1; //change the state of the box
-      lastDebounceTime = millis(); //set the current time
-    }
+      char command = strtok(inputBuffer, " ")[0];  // Take the first character as the command
 
-    if ( (buttonState2 == 1) && (boxState2 < 0) ) {
-      Serial.println("open door 2");
-      myservo2.write(0); //open the box
-      boxState2 = -boxState2; //change the state of box
-      lastDebounceTime = millis(); //set the current time
+      switch (command) {
+        case 'o':  // Open command
+        case 'c':
+          {  // Close command
+            // For these commands, parse additional arguments
+            int boxNumber = atoi(strtok(NULL, " "));  // Parse the box number starting from the third character in the input buffer
+            if (boxNumber >= 1 && boxNumber <= NUMBER_OF_BOXES) {
+              int boxIndex = boxNumber - 1;
+              if (command == 'o') {
+                boxes[boxIndex].servo.write(0);
+                boxes[boxIndex].boxState = 1;
+                Serial.println("{\"code\": 200, \"explain\": \"Success: Opened box " + String(boxNumber) + "\"}");
+              } else {
+                boxes[boxIndex].servo.write(55);
+                boxes[boxIndex].boxState = -1;
+                Serial.println("{\"code\": 200, \"explain\": \"Success: Closed box " + String(boxNumber) + "\"}");
+              }
+            } else {
+              Serial.println("{\"code\": 400, \"explain\": \"Error: Invalid box number\"}");
+            }
+            break;
+          }
+        default:
+          Serial.println("{\"code\": 400, \"explain\": \"Error: Unknown command\"}");
+          break;
       }
-    else if ( (buttonState2 == 1) && (boxState2 > 0) ) {
-      Serial.println("closed door 2");
-      myservo2.write(55); //close the box
-      boxState2 = -boxState2; //change the state of the box
-      lastDebounceTime = millis(); //set the current time
+
+      // Clear the inputBuffer ready for the next command
+      bufferIndex = 0;
+    } else if (bufferIndex < sizeof(inputBuffer) - 1) {
+      // Append the read character to the input buffer
+      inputBuffer[bufferIndex++] = inChar;
+    }
+  }
+
+  for (int i = 0; i < NUMBER_OF_BOXES; i++) {
+    buttons[i].reading = digitalRead(buttons[i].pin);
+
+    if (buttons[i].reading != buttons[i].lastState) {
+      // reset the debouncing timer
+      buttons[i].lastDebounceTime = millis();
     }
 
-    if ( (buttonState3 == 1) && (boxState3 < 0) ) {
-      Serial.println("open door 3");
-      myservo3.write(0); //open the box
-      boxState3 = -boxState3; //change the state of box
-      lastDebounceTime = millis(); //set the current time
+    // Check if debounce delay has passed
+    if ((millis() - buttons[i].lastDebounceTime) > debounceDelay) {
+      if (buttons[i].reading != buttons[i].state) {
+        buttons[i].state = buttons[i].reading;
+        // If the button is pressed and the box is closed...
+        if ((buttons[i].state == 1) && (boxes[i].boxState < 0)) {
+          Serial.print("{\"code\": 201, \"explain\": \"Success: Manually opened box " + String(i + 1) + "\"}");  // print a message to the serial monitor
+          boxes[i].servo.write(0);      // open the box
+          boxes[i].boxState = 1;        // change the state of the box
+          buttons[i].lastDebounceTime = millis();  // update the debounce timer
+        }
+        // If the button is pressed and the box is open...
+        else if ((buttons[i].state == 1) && (boxes[i].boxState > 0)) {
+          Serial.print("{\"code\": 201, \"explain\": \"Success: Manually closed box " + String(i + 1) + "\"}");  // print a message to the serial monitor
+          boxes[i].servo.write(55);     // close the box
+          boxes[i].boxState = -1;       // change the state of the box
+          buttons[i].lastDebounceTime = millis();  // update the debounce timer
+        }
       }
-    else if ( (buttonState3 == 1) && (boxState3 > 0) ) {
-      Serial.println("closed door 3");
-      myservo3.write(55); //close the box
-      boxState3 = -boxState3; //change the state of the box
-      lastDebounceTime = millis(); //set the current time
     }
-
-    if ( (buttonState4 == 1) && (boxState4 < 0) ) {
-
-      Serial.println("open door 4");
-      myservo4.write(0); //open the box
-      boxState4 = -boxState4; //change the state of box
-      lastDebounceTime = millis(); //set the current time
-      
-      }
-    else if ( (buttonState4 == 1) && (boxState4 > 0) ) {
-
-      Serial.println("closed door 4");
-      myservo4.write(55); //close the box
-      boxState4 = -boxState4; //change the state of the box
-      lastDebounceTime = millis(); //set the current time
-    }
-
-    if ( (buttonState5 == 1) && (boxState5 < 0) ) {
-
-      Serial.println("open door 5");
-      myservo5.write(0); //open the box
-      boxState5 = -boxState5; //change the state of box
-      lastDebounceTime = millis(); //set the current time
-      }
-    else if ( (buttonState5 == 1) && (boxState5 > 0) ) {
-      
-      Serial.println("closed door 5");
-      myservo5.write(55); //close the box
-      boxState5 = -boxState5; //change the state of the box
-      lastDebounceTime = millis(); //set the current time
-    }
-
-  }//close if(time buffer)
-
-}//close void loop
+    buttons[i].lastState = buttons[i].reading;
+  }
+}
